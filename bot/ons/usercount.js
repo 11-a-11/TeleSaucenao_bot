dfae3dc6b1d6fc83aab916043c9fc470c4da7d9c
frank kim <idforcoding@gmail.com>
2020-02-25 14:19:23 +0900

    add file send func @ usercount.js

--- bot/ons/usercount.js
+++ bot/ons/usercount.js
@@ -1,4 +1,5 @@
 var tools = require("../../tools/tools.js");
+var fs = require('fs');
 
 module.exports = function(bot, admin) {
   return function(msg) {
@@ -14,10 +15,18 @@ module.exports = function(bot, admin) {
 
     if (global.debug) console.log("msg is ", msg, ids);
 
-    for (var i = 0; i < ids.length; i++) {
-      if (ids[i] === "on") continue;
-      text = text + userCount[ids[i]]["username"] + "\t[" + ids[i] + "]\t" + userCount[ids[i]]["count"] + "\n";
-    }
-    return bot.sendMessage(chat_id, "USERCOUNT:\n" + text, {reply: reply, parse: "Markdown"});
+    fs.writeFile("../../log_usercount/outfile.txt", JSON.stringify(userCount), function(err) {
+      if (err) {
+        if (global.debug) console.log("Error write file @ usercount.jsl");
+      }
+      return bot.sendDocument(chat_id, "../../log_usercount/outfile.txt", {reply: reply, parse: "Markdown"});
+    );
+    
+    //for (var i = 0; i < ids.length; i++) {
+    //  if (ids[i] === "on") continue;
+    //  text = text + userCount[ids[i]]["username"] + "\t[" + ids[i] + "]\t" + userCount[ids[i]]["count"] + "\n";
+    //}
+    //return bot.sendMessage(chat_id, "USERCOUNT:\n" + text, {reply: reply, parse: "Markdown"});
   };
 };
+

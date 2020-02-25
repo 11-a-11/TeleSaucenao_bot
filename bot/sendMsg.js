e7a08bfacf9c6e6c6d7ace6d9ae56d47789a9f18
frank kim <idforcoding@gmail.com>
2020-02-25 20:55:06 +0900

    fix markdown exception @ sendMsg.js

--- bot/sendMsg.js
+++ bot/sendMsg.js
@@ -1,4 +1,6 @@
-// 주의: bot.sendmessage의 text param에 markup기호 외 "[" 및 "]"가 들어가면 에러뜸;; 알아내느라고 2시간 썼다;;
+// 주의: bot.sendmessage의 text param에 markdown 기호 예컨대 [, ], (,), _, `가 온전하게 닫힌 형태로 안 들어가면 에러뜸;; 알아내느라고 2시간썼다;;
+// 예외처리를 하기 위해 \를 붙여야하는데, 하나만 붙였더니 나중에 array.join하는 과정에서 없어지는 건지 다시 에러가 발생.
+// \\ 처럼 두개를 붙여주니 실제 출력될때 \가 하나 붙은 채로 출력. 일단 하는 수 없는 듯.
 
 var urlbase = require("../settings/settings.js").url;
 var MESSAGE = require("../settings/settings.js").msg;
@@ -53,8 +55,13 @@ var sendMsg = function(results, totalLength, bot, msg) {
   for (var i = 0; i < data_keys.length; i++) {
     var key = data_keys[i];
     if (typeof data[key] === "string") {
-      data[key] = data[key].replace(/\[/gi, "(");
-      data[key] = data[key].replace(/\]/gi, ")");
+      data[key] = data[key].replace(/\[/gi, "\\[");
+        data[key] = data[key].replace(/\]/gi, "\\]");
+        data[key] = data[key].replace(/\(/gi, "\\(");
+        data[key] = data[key].replace(/\)/gi, "\\)");
+        data[key] = data[key].replace(/\*/gi, "\\*");
+        data[key] = data[key].replace(/\_/gi, "\\_");
+        data[key] = data[key].replace(/\`/gi, "\\`");
     }
   }
 
